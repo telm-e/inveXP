6db9d32d9e0f7ed3ad486adedb5fe78390419e6b
telm-e <telmaeugenio@gmail.com>
2022-07-22 11:55:10 -0300

    feat: token validation applied in /wallet and /account endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const tokenValidation = require('../middlewares/tokenValidation');
 
 const routes = express.Router();
 
 routes.use('/login', require('./loginRoute'));
-routes.use('/account', require('./accountRoute'));
+routes.use('/account', tokenValidation, require('./accountRoute'));
 routes.use('/asset', require('./assetRoute'));
-routes.use('/wallet', require('./walletRoute'));
+routes.use('/wallet', tokenValidation, require('./walletRoute'));
 
 // routes.use(middlewares.error);
 
