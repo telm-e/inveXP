9788737a9dc4873139b1fe054cc2a3294c601661
telm-e <telmaeugenio@gmail.com>
2022-07-22 11:28:56 -0300

    feat: middleware loginValidation

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const loginController = require('../controllers/loginController');
+const loginValidation = require('../middlewares/loginValidation');
 
 const routes = express.Router();
 
-routes.post('/', loginController.getClient);
+routes.post('/', loginValidation, loginController.getClient);
 
 module.exports = routes;
