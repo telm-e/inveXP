67029e19a156fd6632c38e8859d7917fc49bee56
telm-e <telmaeugenio@gmail.com>
2022-07-22 12:09:30 -0300

    feat: token validation applied in /asset/:clientId

--- src/controllers/assetController.js
+++ src/controllers/assetController.js
@@ -1,14 +1,16 @@
 const assetService = require('../services/assetService');
+const tokenValidation = require('../middlewares/tokenValidation');
 
-const getById = async (req, res) => {
+const getById = async (req, res, next) => {
     const { id } = req.params;
     const [getById] = await assetService.getById(id);
     if (getById.length === 0) {
       return res.status(404).json({ message: 'ID not found' });
     }
-    if (id < 10000) {
+    if (id < 10000) { // assetId case
       return res.status(200).json(getById[0]);
     }
+    tokenValidation(req, res, next); // clientId case
     res.status(200).json(getById);
   };
 
